fa70ec2c2fc3d695a0f50b6af9c286a94fb19ed2
Orveo Di luca <102887303+OrveoDiLuca@users.noreply.github.com>
2024-09-24 21:47:39 -0400

    getters&settersCompany

--- ProyectoSSOO/src/classes/Company.java
+++ ProyectoSSOO/src/classes/Company.java
@@ -12,4 +12,30 @@ public class Company {
     public String nameCompany; 
     public int employeesCantity; //cantidad de empleados. 
     public int computerProfit; //cuanta gana la empresa. 
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    public int getEmployeesCantity() {
+        return employeesCantity;
+    }
+
+    public void setEmployeesCantity(int employeesCantity) {
+        this.employeesCantity = employeesCantity;
+    }
+
+    public int getComputerProfit() {
+        return computerProfit;
+    }
+
+    public void setComputerProfit(int computerProfit) {
+        this.computerProfit = computerProfit;
+    }
+    
+    
 }
